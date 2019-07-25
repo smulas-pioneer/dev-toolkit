01147458304d4038cddb27216541ba859f9291ba
Jd <jodan.pz@gmail.com>
2019-07-25 17:26:03 +0200

    Update create-project.sh

--- create-project/create-project.sh
+++ create-project/create-project.sh
@@ -3,10 +3,16 @@
 name=$1
 template=$2
 
+echo Creating project "$name"...
+
 cp -r /app/template/$template ./$name
 
 shopt -s globstar nullglob
 
+echo Applying substituions to contents...
 sed -i -e "s/__DT_PROJECT_NAME/${name}/g" ./$name/**/*.*
 
-rename "s/__DT_PROJECT_NAME/${name}/g" **/**
+echo Finalizing porject file system...
+#rename "s/__DT_PROJECT_NAME/${name}/g" **/**
+
+echo Done!
