10eb6b5633ad3f266dea2c82f333d5780f747834
Daniele Giove <jodan-pz@gmail.com>
2019-07-26 14:40:00 +0200

    dg

---
+++ create-project/template/netcore/__DT_PROJECT_NAME.Batch/src/DependencyInjection/RootComposition.cs
@@ -0,0 +1,27 @@
+using __DT_PROJECT_NAME.Common;
+using __DT_PROJECT_NAME.Services;
+using DHARMA.Extensions.DependencyInjection;
+using Lamar;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace __DT_PROJECT_NAME.Batch.DependencyInjection {
+    public static class RootComposition {
+        public static IServiceCollection Add__DT_PROJECT_NAME(this ServiceRegistry registry) {
+            registry.ConfigureWithDave<IAppSupport, AppSupport>((_, builder) =>
+             {
+                 builder.Connections.WithCache
+                        .Map("SAMPLE")
+                        .ToProperty("DHARMA_SAMPLE_CONNECTION");
+                 builder.Parameters
+                       .MapSection("sample").Cached.ToProperty("SampleParams")
+                       .MapSection("other").ToRoot();
+
+                 // authorization(s)
+                 //  builder.Authorizations.MapToProperty("Auth");
+             })
+             .AddSingleton<I__DT_PROJECT_NAMEBatch, __DT_PROJECT_NAMEBatch>();
+
+            return registry;
+        }
+    }
+}
---
+++ create-project/template/netcore/__DT_PROJECT_NAME.Services/src/AppSupport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using __DT_PROJECT_NAME.Common;
+using DHARMA.Domain;
+using DHARMA.Jacob.CommonUtils;
+
+namespace __DT_PROJECT_NAME.Services {
+    public class AppSupport : IAppSupport {
+        public DharmaConnection DHARMA_SAMPLE_CONNECTION { get; set; }
+
+        public Dictionary<string, string> SampleParams { get; set; }
+        public IDictionary<string, string> Params => SampleParams;
+
+        public string LoaderWebApiEndpoint { get; set; }
+        public string ServiceCookieEmailValue { get; set; }
+
+        public (string Url, string User, string Password)? SAMPLE_CONNECTION {
+            get {
+                var connection = DHARMA_SAMPLE_CONNECTION;
+                if (connection == null) return null;
+                return (connection.Server,
+                        connection.User,
+                        connection.SecuredData != null ? CryptHelper.Decryptor(connection.SecuredData) : null);
+            }
+        }
+    }
+}
