00b0de83aed87aebb5bd2aba99578ae1f26af512
Daniele Giove <jodan-pz@gmail.com>
2019-07-26 15:14:28 +0200

    dg

--- create-project/create-project.sh
+++ create-project/create-project.sh
@@ -9,19 +9,20 @@ cp -r /app/template/$template ./$name
 
 shopt -s globstar nullglob
 
+cd ./$name
 echo Applying substitutions...
 # sed -i -e "s/__DT_PROJECT_NAME/${name}/g" ./$name/**/(.)
-find ./$name -type f -exec grep -Iq . {} \; -exec sed -i "s/__DT_PROJECT_NAME/${name}/g" {} +
+find . -type f -exec grep -Iq . {} \; -exec sed -i "s/__DT_PROJECT_NAME/${name}/g" {} +
 
 echo Finalizing project...
 # Rename files with __DT_PROJECT_NAME
-for i in ./$name/**/*__DT_PROJECT_NAME*.*
+for i in ./**/*__DT_PROJECT_NAME*.*
 do
     mv "$i" "${i/__DT_PROJECT_NAME/$name}"
 done
 
 # Rename _gitignore into .gitignore
-for i in ./$name/**/_gitignore
+for i in ./**/_gitignore
 do
     mv "$i" "${i/_gitignore/.gitignore}"
 done
