30a10d6c4daecc7fdefb6e1a64eb8cca50d2a122
Daniele Giove <jodan-pz@gmail.com>
2019-07-26 15:31:50 +0200

    dg

--- create-project/create-project.sh
+++ create-project/create-project.sh
@@ -12,7 +12,13 @@ shopt -s globstar nullglob
 cd ./$name
 echo Applying substitutions...
 # sed -i -e "s/__DT_PROJECT_NAME/${name}/g" ./$name/**/(.)
-find . -type f -exec grep -Iq . {} \; -exec sed -i "s/__DT_PROJECT_NAME/${name}/g" {} +
+#find . -type f -exec grep -Iq . {} \; -exec sed -i "s/__DT_PROJECT_NAME/${name}/g" {} +
+
+find . -type f |
+while read filename
+ sed -i "s/__DT_PROJECT_NAME/${name}/g" $filename
+do
+done
 
 echo Finalizing project...
 # Rename files with __DT_PROJECT_NAME
