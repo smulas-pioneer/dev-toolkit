bb56b4061847402596a68d113942304e92286409
Daniele Giove <jodan-pz@gmail.com>
2019-07-26 15:59:01 +0200

    dg

--- build-project/template/react/install.sh
+++ build-project/template/react/install.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname $0)
 
-docker rmi dev-toolkit-build-project-react
-docker build -t dev-toolkit-build-project-react .
+if [[ "$(docker images -q dev-toolkit-build-project-react 2> /dev/null)" == "" ]]; then
+  docker rmi dev-toolkit-build-project-react
+fi
+docker build  --quiet -t dev-toolkit-build-project-react . > /dev/null
 
