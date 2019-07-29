abe714d059e88712745e03539a5dce623e7e8809
Daniele Giove <jodan-pz@gmail.com>
2019-07-29 17:10:18 +0200

    dg

--- build-project/install.sh
+++ build-project/install.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)
 
 ./template/react/install.sh
-# ./template/netcore/install.sh
+./template/netcore/install.sh
 
 cp ./bin/* /usr/local/bin
--- build-project/template/netcore/install.sh
+++ build-project/template/netcore/install.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-if [[ "$(docker images -q dev-toolkit-build-project-netcore 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q dev-toolkit-build-project-netcore 2> /dev/null)" != "" ]]; then
   docker rmi dev-toolkit-build-project-netcore
 fi
 docker build  --quiet -t dev-toolkit-build-project-netcore . > /dev/null
