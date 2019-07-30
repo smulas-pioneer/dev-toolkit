bb21088e915a0a492e66af8b00bcde05c4691dcb
Daniele Giove <jodan-pz@gmail.com>
2019-07-30 10:04:43 +0200

    dg

--- build-project/template/netcore/build-project-netcore.sh
+++ build-project/template/netcore/build-project-netcore.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 echo Build .NETCORE Project
 
+no_restore=
+
 if [ -d "$DT_PROJECT" ]; then
   cd $DT_PROJECT
+
+  if [ -f "../dt-build-project-nuget.config" ]; then
+    dotnet restore --configfile ../dt-build-project-nuget.config
+    no_restore="--no-restore"
+  fi
+
+else
+
+  if [ -f "dt-build-project-nuget.config" ]; then
+    dotnet restore --configfile dt-build-project-nuget.config
+    no_restore="--no-restore"
+  fi
+
 fi
 
-dotnet publish -c release -o ./build
+dotnet publish -c release $no_restore -o ./build
 
 echo Done!
