b9b56a760b6aea7efe1275d9defd14ef7f17122c
Daniele Giove <daniele.giove@amundi.com>
2020-02-20 11:21:48 +0100

    dg

--- create-api-client/tpl/cs/template.cs
+++ create-api-client/tpl/cs/template.cs
@@ -14,32 +14,30 @@ public class ApiConfiguration {
 
 public class BaseClient {
     readonly ApiConfiguration _config;
-    HttpClient _client;
+
     public BaseClient(ApiConfiguration config) {
         _config = config;
     }
 
     protected async Task<HttpClient> CreateHttpClientAsync(CancellationToken cancellation) {
-        if (_client == null) {
-            var clientHandler = new HttpClientHandler
-            {
-                Credentials = _config.ClientCredentials
-            };
-            if (_config.UseCookies) {
-                clientHandler.UseCookies = true;
-                clientHandler.CookieContainer = new CookieContainer();
-            }
-            if (_config.UseProxy && _config.ProxyAddress != null) {
-                clientHandler.UseProxy = true;
-                Uri newUri = new Uri(_config.ProxyAddress);
-                var proxy = new WebProxy(newUri)
-                {
-                    Credentials = _config.ProxyCredentials
-                };
-                clientHandler.Proxy = proxy;
-            }
-            _client = new HttpClient(clientHandler);
+
+        var clientHandler = new HttpClientHandler
+        {
+            Credentials = _config.ClientCredentials
+        };
+        if (_config.UseCookies) {
+            clientHandler.UseCookies = true;
+            clientHandler.CookieContainer = new CookieContainer();
         }
-        return _client;
+        if (_config.UseProxy && _config.ProxyAddress != null) {
+            clientHandler.UseProxy = true;
+            Uri newUri = new Uri(_config.ProxyAddress);
+            var proxy = new WebProxy(newUri)
+            {
+                Credentials = _config.ProxyCredentials
+            };
+            clientHandler.Proxy = proxy;
+        }
+        return new HttpClient(clientHandler);
     }
 }
