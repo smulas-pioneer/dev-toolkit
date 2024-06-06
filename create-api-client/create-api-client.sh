c5e7fae64737867b2488a26c6eb9b937b4f3b838
Daniele Giove <daniele.giove@gmail.com>
2024-06-06 13:24:46 +0200

    upd

--- create-api-client/create-api-client.sh
+++ create-api-client/create-api-client.sh
@@ -4,14 +4,16 @@
 function usage()
 {
    cat << HEREDOC
+   [June 2024]
+   Usage: dt-$progname [--swag SWAGGER_ENDPOINT] [--lang LANG] [--output OUTPUT_FILE] ...
 
-   Usage: dt-$progname [--swag SWAGGER_ENDPOINT] [--lang LANG] [--output OUTPUT_FILE] [--namespace C# NAMESPACE]
-
-   optional arguments:
+   available arguments:
      -h, --help                         show this help message and exit
-     -s, --swag       SWAGGER_ENDPOINT  pass a valid endpoint to a swagger file
-     -l, --lang       LANGUAGE          pass 'ts' for typescript, 'cs' for C#
-     -n, --namespace  C# NAMESPACE      pass a valid c# namespace name to be used in the output
+     -s, --swag       SWAGGER_ENDPOINT  a valid endpoint to a swagger file
+     -l, --lang       LANGUAGE          use 'ts' for typescript, 'cs' for C#
+     -n, --namespace  C# NAMESPACE      a valid c# namespace name to be used in the output
+     -x, --nocors                       omit cross-origin-scripting (ts)
+     -r, --optionals                    optional parameters with reorder (required first, optional at the end)
      -o, --output     OUTPUT_FILE       output file
      -d, --debug                        show debug informations
 
@@ -23,17 +25,20 @@ progname=$(basename $0 | cut -d. -f1)
 swag=
 lang=
 ns="MGLib"
+nocors=
+optionals=
 vlang=
 slang=
 output=
 tplExtCode=
 swagCodeOpts=
+swagCodeOptsOptionals=
 debug=
 
 # use getopt and store the output into $OPTS
 # note the use of -o for the short options, --long for the long name options
 # and a : for any option that takes a parameter
-OPTS=$(getopt -o "hs:l:n:o:d" --long "help,swag:,lang:,namespace:,output:,debug" -n "$progname" -- "$@")
+OPTS=$(getopt -o "hs:l:n:xro:d" --long "help,swag:,lang:,namespace:,nocors,optionals,output:,debug" -n "$progname" -- "$@")
 if [ $? != 0 ] ; then echo "Error in command line arguments." >&2 ; usage; exit 1 ; fi
 if [ $# -eq 0 ]; then usage; exit 1; fi
 
@@ -47,6 +52,8 @@ while true; do
     -s | --swag ) swag="$2"; shift 2 ;;
     -l | --lang ) lang="$2"; shift 2 ;;
     -n | --namespace ) ns="$2"; shift 2;;
+    -x | --nocors ) nocors="true"; shift;;
+    -r | --optionals ) optionals="true"; shift;;
     -o | --output ) output="$2"; shift 2 ;;
     -d | --debug ) debug="true"; shift;;
     -- ) shift; break ;;
@@ -54,22 +61,47 @@ while true; do
   esac
 done
 
-tplExtCode="/app/tpl/${lang}/template.${lang}"
+if [ "$nocors" == "true" ]; then
+  tplExtCode="/app/tpl/${lang}/template-no-cors.${lang}"
+else
+  tplExtCode="/app/tpl/${lang}/template.${lang}"
+fi
+
+if [ "$optionals" == "true" ]; then
+  swagCodeOptsOptionals="/GenerateOptionalParameters:true"
+fi
 
 if [[ "$output" == "" ]]; then output="$PWD/$(basename $swag)_client.$lang"; fi
 
 if [[ "$swag" != "" ]] && [[ "$lang" != "" ]]; then
   if [ "$lang" == "ts" ]; then
     vlang="Type Script"
-    slang="swagger2tsclient"
-    swagCodeOpts='/ClassName:{controller}Client /NullValue:Undefined /UseTransformOptionsMethod:true /ClientBaseClass:BaseClientProxy /Template:Fetch /PromiseType:Promise /DateTimeType:string /GenerateClientClasses:true /GenerateDtoTypes:true /OperationGenerationMode:MultipleClientsFromOperationId /MarkOptionalProperties:true /TypeStyle:Interface'
-    swagCodeOpts="$swagCodeOpts /ExtensionCode:$tplExtCode"
+    slang="openapi2tsclient"
+    swagCodeOpts='/ClassName:{controller}Client     
+                  /UseTransformOptionsMethod:true 
+                  /ClientBaseClass:BaseClientProxy 
+                  /Template:Fetch 
+                  /PromiseType:Promise 
+                  /DateTimeType:string 
+                  /GenerateClientClasses:true 
+                  /GenerateDtoTypes:true 
+                  /OperationGenerationMode:MultipleClientsFromOperationId 
+                  /MarkOptionalProperties:true 
+                  /TypeStyle:Interface'
+    swagCodeOpts="$swagCodeOpts $swagCodeOptsOptionals /ExtensionCode:$tplExtCode"
 
   else
     if [ "$lang" == "cs" ]; then
       vlang="C#"
-      slang="swagger2csclient"
-      swagCodeOpts="/Namespace:$ns /ClassStyle:Poco /ArrayType:System.Collections.Generic.IEnumerable /UseHttpClientCreationMethod:true /InjectHttpClient:false /ConfigurationClass:${ns}.ApiConfiguration /ClientBaseClass:BaseClient"
+      slang="openapi2csclient"
+      swagCodeOpts="/Namespace:$ns 
+                    /ClassStyle:Poco 
+                    /ArrayType:System.Collections.Generic.IEnumerable 
+                    /UseHttpClientCreationMethod:true 
+                    /InjectHttpClient:false 
+                    /ConfigurationClass:${ns}.ApiConfiguration 
+                    /ClientBaseClass:BaseClient 
+                    $swagCodeOptsOptionals"
     fi
   fi
 fi
